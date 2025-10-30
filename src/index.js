77829a8e7109c533b4a4829b3f07ac02482d7585
James Cao <jcao5360@gmail.com>
2025-10-30 13:39:10 -0400

    updated format to MVC

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import "./styles.css";
-import { greeting } from "./greeting.js";
-import exampleImage from "./img/example.jpeg";
+import { init } from "./controller.js";
 
-console.log(greeting);
-
-const image = document.createElement("img");
-image.src = exampleImage;
-   
-document.body.appendChild(image);
+init();
